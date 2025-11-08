138fe3fe7f23cffbfcf06935428af8dd03ae8e38
Clément Dev <clement.vigouroux@gmail.com>
2025-11-08 12:07:08 +0100

    fix : delete AutoA11y part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Skills from "./components/Skills"
 import Projects from "./components/Projects"
-import AutoA11y from "./components/AutoA11y"
 import WhyA11y from "./components/WhyA11y"
 import Alternance from "./components/Alternance"
 import Contact from "./components/Contact"
@@ -15,7 +14,6 @@ export default function Home() {
       <Hero />
       <Skills />
       <Projects />
-      <AutoA11y />
       <WhyA11y />
       <Alternance />
       <Contact />
